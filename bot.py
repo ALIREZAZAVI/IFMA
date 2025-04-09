89d32450e2209ce24cbae2470e571819444e6201
ALIREZAZAVI <168830744+ALIREZAZAVI@users.noreply.github.com>
2025-04-09 14:58:20 +0330

    taghirate kossher

--- bot.py
+++ bot.py
@@ -273,11 +273,11 @@ def job_group_5():
 # Schedule jobs
 # schedule.every(1).hour.do(job_group_1)  # Every hour
 # schedule.every(5).minutes.do(job_group_1) 
-# schedule.every(5).seconds.do(job_group_1) 
+schedule.every(5).seconds.do(job_group_1) 
 schedule.every(5).seconds.do(job_group_2) 
 schedule.every(5).seconds.do(job_group_3) 
 schedule.every(5).seconds.do(job_group_4) 
-# schedule.every(5).seconds.do(job_group_5) 
+schedule.every(5).seconds.do(job_group_5) 
 
 
 
